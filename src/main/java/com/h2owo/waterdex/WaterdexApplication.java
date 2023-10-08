20ee120865c9c83da0e12e79123830d2eb10d27a
hector.trejo <hector.trejo@elektra.com.mx>
2023-10-07 18:13:44 -0600

    Initial commit

---
+++ src/main/java/com/h2owo/waterdex/WaterdexApplication.java
@@ -0,0 +1,13 @@
+package com.h2owo.waterdex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterdexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterdexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/h2owo/waterdex/WaterdexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2owo.waterdex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterdexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
